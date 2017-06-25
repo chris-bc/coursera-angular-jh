dcea800986c553c5cda723949779b26d280fbb6e
Chris Bennetts-Cash <chris@bennettscash.id.au>
2017-06-25 18:00:22 +1000

    lec 20 - remove items from list

--- lecture20/app.js
+++ lecture20/app.js
@@ -24,6 +24,10 @@ function ShoppingListShowController(ShoppingListService) {
   var showList = this;
 
   showList.items = ShoppingListService.getItems();
+
+  showList.removeItem = function (itemIndex) {
+    ShoppingListService.removeItem(itemIndex);
+  };
 }
 
 
@@ -43,6 +47,10 @@ function ShoppingListService() {
 
   service.getItems = function () {
     return items;
+  };
+
+  service.removeItem = function (itemIndex) {
+    items.splice(itemIndex, 1);
   };
 }
 
